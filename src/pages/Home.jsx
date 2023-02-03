cfcc0392ec6a7f543e5ea6253e6080165414d7e7
n1kushachh <niikusha420@gmail.com>
2023-02-03 13:08:40 +0400

    added assets, finished homepage

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,20 @@
-import React from 'react'
-import RedberryLogo from "../assets/redberry-logo.svg"
-import RedberryAgency from "../assets/redberry-agency.svg"
+import React from "react";
+import RedberryLogo from "../assets/redberry-logo.svg";
+import RedberryAgency from "../assets/redberry-agency.svg";
 
 export const Home = () => {
   return (
-    <div className='bg-full'>
-      
+    <div className="bg-full">
+      <div className="container">
+        <div className="top">
+          <img className="redberryLogo" src={RedberryLogo}></img>
+          <hr className="line-home"></hr>
+        </div>
+        <div className="mid">
+          <img className="mid-img" src={RedberryAgency}></img>
+          <button className="addres-btn">რეზიუმეს დამატება</button>
+        </div>
+      </div>
     </div>
-  )
-}
-
+  );
+};
