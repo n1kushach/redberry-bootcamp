ffb2e85789833407fed507ea63cea8df3c2e1fa3
n1kushachh <niikusha420@gmail.com>
2023-02-03 12:42:30 +0400

    added homepage bg, started working with routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,17 @@
-import './App.css'
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Home } from "./pages/Home";
 
 function App() {
-
   return (
     <div className="App">
       <Router>
         <Routes>
-          <Route path="/" element={<Home/>}/>
+          <Route path="/" element={<Home />} />
         </Routes>
       </Router>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import RedberryLogo from "../assets/redberry-logo.svg"
+import RedberryAgency from "../assets/redberry-agency.svg"
+
+export const Home = () => {
+  return (
+    <div className='bg-full'>
+      
+    </div>
+  )
+}
+
